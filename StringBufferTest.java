edbdcbd96a38f10cdcdec6087b8b894908a15fd2
Kalpesh Oswal <93819999+kalpesh-oswal@users.noreply.github.com>
2022-11-06 22:56:24 +0530

    Update StringBufferTest.java

--- StringBufferTest.java
+++ StringBufferTest.java
@@ -39,6 +39,6 @@ public class StringBufferTest {
 		sb3.insert(0, "Hello SB3");
 		System.out.println("SB3 hashcode: "+sb3.hashCode());
 		
-		System.out.println("only commit");
+		System.out.println("pull from remote repo");
 	}
 }
